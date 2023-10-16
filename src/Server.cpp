bfea3842dea956c261b9f3d9a36303c1f5b62204
Arist937 <Aristotle937@gmail.com>
2023-10-15 23:10:55 -0400

    added handling of concurrent clients using select syscall

--- src/Server.cpp
+++ src/Server.cpp
@@ -8,8 +8,9 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/select.h>
 
-std::string recieve(int fd);
+int accept_new_connection(int server_fd);
 
 int main(int argc, char **argv) {  
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,7 +26,7 @@ int main(int argc, char **argv) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -41,28 +42,56 @@ int main(int argc, char **argv) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
-  
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "Client connection failed\n";
-    return 1;
-  }
-  std::cout << "Client connected\n";
-  
-  char buffer[1024];
-  while(recv(client_fd, buffer, 1024, 0) > 0) {
-    // Sending PONG to client
-    if (send(client_fd, "+PONG\r\n", 7, 0) < 0) {
-      std::cerr << "Failed to send to socket\n";
+
+  // Event loop begins
+  fd_set current, ready;
+  FD_ZERO(&current);
+  FD_SET(server_fd, &current);
+
+  while (true) {
+    ready = current;
+
+    if (select(FD_SETSIZE, &ready, nullptr, nullptr, nullptr) < 0) {
+      std::cerr << "select failed\n";
+      return 1;
+    }
+
+    for (unsigned int i = 0; i < FD_SETSIZE; ++i) {
+      if (FD_ISSET(i, &ready)) {
+        if (i == server_fd) {
+          int client_fd = accept_new_connection(server_fd);
+          if (client_fd < 0) {
+            std::cerr << "Client connection failed\n";
+            return 1;
+          }
+
+          FD_SET(client_fd, &current);
+          std::cout << "Client connected\n";
+        } else {
+          char buffer[1024];
+          if(recv(i, buffer, 1024, 0) > 0) {
+            // Sending PONG to client
+            if (send(i, "+PONG\r\n", 7, 0) < 0) {
+              std::cerr << "Failed to send to socket\n";
+            }
+          }
+
+          std::cout << "Client requests handled\n";
+        }
+      }
     }
   }
 
   close(server_fd);
 
   return 0;
+}
+
+int accept_new_connection(int server_fd) {
+  std::cout << "Waiting for a client to connect...\n";
+
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+
+  return accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 }
